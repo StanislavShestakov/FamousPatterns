fe19024c8e48c0eecc6fea34ba21b2f2a6ecaa5b
Stas <w3oks@mail.ru>
2019-05-22 17:12:50 +0400

    implement  Strategy

--- src/main/java/Strategy.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Stas on 22.05.2019.
- */
-public class Strategy {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/main/java/StrategyTest.java
@@ -0,0 +1,39 @@
+/**
+ * Created by Stas on 22.05.2019.
+ */
+public class StrategyTest {
+    public static void main(String[] args) {
+        DoorA door = new DoorA();
+
+        Strategy open = new OpenA();
+        door.setStrategy(open);
+        door.move();
+
+        Strategy close = new CloseA();
+        door.setStrategy(close);
+        door.move();
+
+    }
+}
+class DoorA{
+    private  Strategy strategy;
+    public void setStrategy(Strategy strategy){
+        this.strategy = strategy;
+    }
+    public  void  move(){
+        strategy.openClose();
+    }
+}
+interface Strategy{
+    void openClose();
+}
+class OpenA implements  Strategy{
+    public void openClose() {
+        System.out.println("Door is closed!");
+    }
+}
+class CloseA implements Strategy{
+    public void openClose() {
+        System.out.println("Door is open!");
+    }
+}
